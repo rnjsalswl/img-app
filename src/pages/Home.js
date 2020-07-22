dd749ddeb39f341cb06688b17ec28ef15225c5a8
권민지 <minji@gwonminjiui-MacBookPro.local>
2020-07-23 01:31:52 +0900

    masonry layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import './Home.css'
+import img1 from './img/IMG_3135.jpeg'
+import img2 from './img/IMG_3137.jpeg'
 
 const Home = () =>{
     return(
-        <div>
-            <h1>Home</h1>
+        <>
+        <div className="columns">
+            <figure>
+                <img src={img1} />
+                <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
+            </figure>
+            <figure>
+                <img src={img2} />
+                <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
+            </figure>
+            <figure>
+                <img src={img1} />
+                <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
+            </figure>
+            <figure>
+                <img src={img2} />
+                <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
+            </figure>
+            <figure>
+                <img src={img1} />
+                <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
+            </figure>
+            <figure>
+                <img src={img2} />
+                <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
+            </figure>
+            <figure>
+                <img src={img1} />
+                <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
+            </figure>
+            <figure>
+                <img src={img2} />
+                <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
+            </figure>
         </div>
+        </>
     )
 }
 
